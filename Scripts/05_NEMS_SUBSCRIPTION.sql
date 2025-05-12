eec9516e0411df9a39f1677e8e98d2595dd39423
sophierogers22 <sophie.rogers22@nhs.net>
2025-05-12 11:15:21 +0100

    updates to NEMS subscription table
    updates to NEMS subscription table
    'updated' to 'update'
    subscription id from bigint to uniqueidentifer

--- Scripts/05_NEMS_SUBSCRIPTION.sql
+++ Scripts/05_NEMS_SUBSCRIPTION.sql
@@ -2,9 +2,9 @@
 /* Table: NEMS_SUBSCRIPTION                                     */
 /*==============================================================*/
 CREATE TABLE NEMS_SUBSCRIPTION (
-   SUBSCRIPTION_ID      BIGINT               NOT NULL,
+   SUBSCRIPTION_ID      UNIQUEIDENTIFIER     NOT NULL,
    NHS_NUMBER           BIGINT               NOT NULL,
    RECORD_INSERT_DATETIME DATETIME             NULL,
-   RECORD_UPDATED_DATETIME DATETIME             NULL,
+   RECORD_UPDATE_DATETIME DATETIME             NULL,
    CONSTRAINT PK_NEMS_SUBSCRIPTION PRIMARY KEY (SUBSCRIPTION_ID)
 );
